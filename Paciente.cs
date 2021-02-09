34d39601b5219b9be19f4969a83d783aeb964752
Henrique Lustosa <henriqlustosa@gmail.com>
2021-02-09 18:29:32 -0300

    Busca de dados da API cadastro do paciente

---
+++ Paciente.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web
+{
+    public class Paciente
+    {
+        public string Cd_prontuario { get; set; }
+        public string cd_codigo { get; set; }
+        public string Nm_situacao { get; set; }
+        public string Nm_nome { get; set; }
+        public string Nm_nome_social { get; set; }
+        public string Nm_vinculo { get; set; }
+        public string Nm_orgao { get; set; }
+        public string Cd_rf_matricula { get; set; }
+        public string In_sexo { get; set; }
+        public string Dc_cor { get; set; }
+        public string Dc_estado_civil { get; set; }
+        public string Cd_mae { get; set; }
+        public string Nm_mae { get; set; }
+        public string Nm_pai { get; set; }
+        public string Dt_data_nascimento { get; set; }
+        public string Nr_idade { get; set; }
+        public string Nm_nacionalidade { get; set; }
+        public string Nm_naturalidade { get; set; }
+        public string Sg_uf { get; set; }
+        public string Dc_grau_instrucao { get; set; }
+        public string Dc_ocupacao { get; set; }
+        public string Nr_ddd_fone { get; set; }
+        public string Nr_fone { get; set; }
+        public string Nr_ddd_fone_recado { get; set; }
+        public string Nr_fone_recado { get; set; }
+        public string Cd_cep { get; set; }
+        public string Dc_logradouro { get; set; }
+        public string Nr_logradouro { get; set; }
+        public string Dc_complemento_logradouro { get; set; }
+        public string Dc_bairro { get; set; }
+        public string Cd_ibge_cidade { get; set; }
+        public string Sg_uf_endereco { get; set; }
+        public string Tp_enderec { get; set; }
+        public string In_correspondencia { get; set; }
+        public string Nr_rg { get; set; }
+        public string Dc_orgao_emissor { get; set; }
+        public string Sg_uf_sigla_emitiu_docto { get; set; }
+        public string Dt_emissao_documento { get; set; }
+        public string Nr_cpf { get; set; }
+        public string Nr_pis { get; set; }
+        public string In_documentos_apresntados { get; set; }
+        public string Nm_certidao { get; set; }
+        public string Nm_cartorio { get; set; }
+        public string Nr_livro { get; set; }
+        public string Nr_folhas { get; set; }
+        public string Nr_termo { get; set; }
+        public string Dt_emissao { get; set; }
+        public string Nr_declaracao_nascido { get; set; }
+        public string Nr_cartao_saude { get; set; }
+        public string Nm_motivo_cadastro { get; set; }
+        public string Dc_documento_referencia { get; set; }
+        public string Nr_cartao_nacional_saude_mae { get; set; }
+        public string Dt_entrada_br { get; set; }
+        public string Dt_naturalizacao { get; set; }
+        public string Nr_portaria { get; set; }
+        public string Dc_observacao { get; set; }
+        public string Situacao_vinculo { get; set; }
+        public string Causa_termino { get; set; }
+        public string Validade_termino { get; set; }
+        public string Grau_dep_vinculo { get; set; }
+        public string Nome_titular { get; set; }
+        public string Ddd_fone_comercial { get; set; }
+        public string Fone_comercial { get; set; }
+        public string Email { get; set; }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace web
 {
     public class Startup
     {
+
+       
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -20,14 +25,50 @@ namespace web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            Paciente details = null;
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            try
+            {
+                
+                string URI = "http://intranethspm:5003/hspmsgh-api/pacientes/paciente/11209913";
+                WebRequest request = WebRequest.Create(URI);
 
+                HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(URI);
+                // Sends the HttpWebRequest and waits for a response.
+                HttpWebResponse httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+
+                if (httpResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    var reader = new StreamReader(httpResponse.GetResponseStream());
+
+                    JsonSerializer json = new JsonSerializer();
+
+                    var objText = reader.ReadToEnd();
+
+                  details = JsonConvert.DeserializeObject<Paciente>(objText);
+
+
+            
+
+
+                    // GridInternado.DataSource = details; // apresentação dos dados da lista
+                    //  GridInternado.DataBind();
+                }
+
+
+            }
+
+            catch (WebException ex)
+            {
+                string err = ex.Message;
+            }
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Olá mundo!!");
+
+                await context.Response.WriteAsync( $"{details.Cd_prontuario, -10}{details.Nm_nome, -40}{details.Email}{details.In_sexo, 5}");
             });
         }
     }
